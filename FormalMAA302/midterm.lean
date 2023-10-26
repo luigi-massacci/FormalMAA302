986fe2b9f2f057e2a8840f2d61f47977aa998afe
Luigi Massacci <luigi.massacci.it@gmail.com>
2023-10-26 15:16:12 +0200

    Midterm III compactness

--- FormalMAA302/midterm.lean
+++ FormalMAA302/midterm.lean
@@ -18,7 +18,7 @@ lemma O_is_openEmpty : ∅ ∈ O := by
   rw [O]; left; rfl
 
 lemma O_is_openX : univ ∈ O := by
-  simp [O]
+  simp [O] -- worth it to have simp? [O] as example for PRL
 
 lemma O_isOpen_sUnion : ∀ C : Set (Set X), (∀ (U : Set X), U ∈ C → U ∈ O) → (⋃₀ C) ∈ O := by
   intro C hC
@@ -57,6 +57,62 @@ instance : TopologicalSpace X := by
   exact O_is_openX
   exact O_is_openInter
   exact O_isOpen_sUnion
+
+example : CompactSpace X := by
+  constructor
+  apply isCompact_of_finite_subcover
+  intro I cover open_of_cover is_cover
+  by_cases emptyCover : ⋃ i : I, (cover i) = ∅
+  · use ∅
+    rw [emptyCover] at is_cover
+    simp [is_cover]
+  · obtain ⟨i, hi⟩ := nonempty_iUnion.mp (nmem_singleton_empty.mp emptyCover)
+    have hfinite : Set.Finite ((cover i)ᶜ) := by
+      have : (cover i) ∈ O := by exact open_of_cover i
+      cases' this with empty cofinite
+      exfalso
+      apply nmem_singleton_empty.mpr hi
+      assumption
+      assumption
+    have h : ∀ x ∈ (cover i)ᶜ, ∃ j : I, x ∈ cover j := by
+      intro x hx
+      have : x ∈ univ := by exact trivial
+      have : x ∈  ⋃ (i : I), cover i := by exact is_cover this
+      exact mem_iUnion.mp this
+    have : Nonempty I := by exact Nonempty.intro i
+    choose! f h using h
+    set subcover := {i} ∪  (f '' ((cover i)ᶜ)) with hsub
+    have : Set.Finite subcover := by
+      rw [hsub]
+      apply finite_union.mpr
+      constructor
+      simp only [finite_singleton]
+      exact Finite.image f hfinite
+    use (Finite.toFinset this)
+    intro x hx
+    by_cases hn : Set.Nonempty ((cover i)ᶜ)
+    by_cases x ∈ (cover i)
+    apply mem_iUnion.mpr
+    use i
+    simp [h]
+    simp [h]
+    aesop
+    by_cases x ∈ (cover i)
+    simp [h]
+    have : (cover i)ᶜ = ∅ := by exact not_nonempty_iff_eq_empty.mp hn
+    have : x ∉ (cover i)ᶜ := by exact not_of_eq_false (congrArg (Membership.mem x) this)
+    contradiction
+
+
+example (hinf : Infinite X): ¬(T2Space X) := by
+  intro HaussX
+  have := (t2Space_iff X).mp HaussX
+  sorry
+
+
+
+
+
 
 
 
