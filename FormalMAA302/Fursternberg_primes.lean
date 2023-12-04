c676b3816a1f29f6315f5048330713a98298fde5
Luigi Massacci <luigi.massacci.it@gmail.com>
2023-12-04 10:00:24 +0100

    Furstenberg topology yeaaaaah

--- FormalMAA302/Fursternberg_primes.lean
+++ FormalMAA302/Fursternberg_primes.lean
@@ -1,9 +1,25 @@
 import Mathlib.Tactic
 
 
-def S (a b : ℤ ) := {n | n % a = b}
+def S (a b : ℤ) := {n | a ∣ n - b }
 
-def O : Set (Set ℤ) := {∅} ∪ { U | ∀n ∈ U, ∃ m : ℤ, 1 ≤ m ∧ S m n ⊆ U }
+
+example (n : ℤ) : Even n ↔  n ∈ S 2 0 := by
+  constructor
+  · intro h
+    simp [S]
+    rcases h with ⟨k, hk⟩
+    rw [hk]
+    ring_nf
+    simp
+  · intro h
+    simp [S] at h
+    rcases h with ⟨w, hw⟩
+    use w
+    rw [hw]
+    ring
+
+def O : Set (Set ℤ) := {∅} ∪ { U | ∀n ∈ U, ∃ m : ℤ, 1 ≤ m ∧ S m n ⊆ U}
 
 open Int
 
@@ -33,6 +49,7 @@ lemma O_isOpen_sUnion : ∀ C : Set (Set ℤ), (∀ (U : Set ℤ), U ∈ C → U
   rfl
   assumption
 
+
 lemma O_is_openInter : ∀ U V : Set ℤ , U ∈ O → V ∈ O → U ∩ V ∈ O:= by
   simp [O]
   intro U V hU hV n nU nV
@@ -40,10 +57,35 @@ lemma O_is_openInter : ∀ U V : Set ℤ , U ∈ O → V ∈ O → U ∩ V ∈ O
   rcases hV n nV with ⟨v, hv⟩
   use u*v
   constructor
-  apply mul_le_mul (hu.1) (hv.1) (by linarith) (by linarith [hu.1])
-  constructor
+  rw [← one_mul 1]
+  have : 0 ≤ u := by
+    apply le_trans
+    exact ((show 0 ≤ 1 by norm_num))
+    exact hu.1
+  apply mul_le_mul (hu.1) (hv.1) (by norm_num) this
   simp [S]
-  simp [S] at hu
+  constructor
   refine subset_trans ?_ hu.2
-  simp
-  intro a ha
+  simp [S]
+  intro a ⟨k, hk⟩
+  use v*k
+  ring_nf
+  assumption
+  refine subset_trans ?_ hv.2
+  simp [S]
+  intro a ⟨k, hk⟩
+  use u*k
+  ring_nf
+  rw [mul_comm v u]
+  assumption
+
+
+lemma infinite_s (a b : ℤ): Set.Infinite (S a b) := by
+  refine Set.infinite_of_not_bddAbove ?_
+  intro h
+  cases' h with w hw
+  have : a*w + b ∈ S a b := by
+    simp [S]
+  by_cases h : 0 < b
+  sorry
+  sorry
